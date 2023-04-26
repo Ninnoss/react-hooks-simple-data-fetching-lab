d50e530698d3d20c1514e7d738545216f63635ce
Ninos Dinkha <ninos.dinkha97@gmail.com>
2023-04-26 18:17:01 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, { useEffect, useState } from 'react';
+
+const App = () => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [image, setImage] = useState('');
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setImage(data.message);
+        setIsLoaded(true);
+      });
+  }, []);
+
+  if (!isLoaded) return <h3>Loading...</h3>;
+
+  return (
+    <img
+      src={image}
+      alt="A Random Dog"></img>
+  );
+};
+
+export default App;
